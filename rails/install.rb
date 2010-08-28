dc7eeb863c045d8a6a7b047dd85af03da3a317dd
Scott Steadman <ss@stdmn.com>
2010-08-28 09:01:08 -0700

    Make sure db/migrate exists before copying to it.

--- rails/install.rb
+++ rails/install.rb
@@ -1,8 +1,9 @@
 
 # copy migrations
+FileUtils.mkdir_p("#{Rails.root}/db/migrate")
 Dir["#{Rails.root}/vendor/plugins/simple_metrics_engine/db/migrate/*.rb"].each do |file|
   puts "Copying #{File.basename(file)} to db/migrate"
-  FileUtils.cp(file, "#{Rails.root}/db/migrate", :verbose => true)
+  FileUtils.cp(file, "#{Rails.root}/db/migrate")
 end
 
 sh 'rake db:migrate'
