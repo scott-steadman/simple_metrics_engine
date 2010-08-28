e931a1998f8610dc5a012d90931f963c8f5fc856
Scott Steadman <ss@stdmn.com>
2010-08-28 08:56:34 -0700

    Added verbose flag.

--- rails/install.rb
+++ rails/install.rb
@@ -2,7 +2,7 @@
 # copy migrations
 Dir["#{Rails.root}/vendor/plugins/simple_metrics_engine/db/migrate/*.rb"].each do |file|
   puts "Copying #{File.basename(file)} to db/migrate"
-  FileUtils.cp(file, "#{Rails.root}/db/migrate")
+  FileUtils.cp(file, "#{Rails.root}/db/migrate", :verbose => true)
 end
 
 sh 'rake db:migrate'
